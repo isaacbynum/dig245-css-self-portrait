8cf6d9abea36efb63ae377982e41d570a90da232
isaacbynum <112572968+isaacbynum@users.noreply.github.com>
2022-09-18 22:50:25 -0400

    Finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,13 @@ Staaatliches Bauhaus (1919-1933)
 `;
 
 console.log(bauhaus);
+
+// const matches = document.querySelectorAll("div.five, div.six, div.seven, div.eight, div.nine, div.ten, div.eleven, div.twelve, div.thirteen, div.fourteen, div.fifteen");
+const matches = document.querySelectorAll("div");
+
+console.log(matches);
+
+document.addEventListener('mousemove', (event) => {
+  console.log(`Mouse X: ${event.clientX}, Mouse Y: ${event.clientY}`);
+
+});
